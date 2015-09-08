b641cf32c98dd463335a0a302eac4c2ff0528fac
VM-HOME <stathis@chrysikos.gr>
2015-09-08 20:04:23 +0300

    Comments
    Comments sthn Class CustomersFunctions

--- OrderSystem/CustomersFunctions.cs
+++ OrderSystem/CustomersFunctions.cs
@@ -9,6 +9,16 @@ namespace OrderSystem
 {
     class CustomersFunctions
     {
+        /*
+         * Edo anti na valeis olo to path mporeis na grapseis to ekseis:
+         * @"filename.extension" ean grapseis me tin morfi auth xoris na valeis path 
+         * mono diladi to onoma tou arxeiou me to extension to arxeio tha dimiourgithei 
+         * mesa sto fakelo pou tha egkatastathei to programma tora otan esy tha trexeis
+         * to programma apo to visual studio patontas F5 to arxeio tha apothikevete mesa
+         * sto fakelo bin tou project gia na pas grigora sto fakelo mesa apo to visual studio
+         * patas deksi click pano sto onoma tou project apo to solution explorer kai meta click
+         * sto open Folder in File Explorer
+         */
         private string FilePath = @"C:\Users\Giannis\Documents\GitHub\OrderSystem\Data";
         private void CheckFileExist(string FilePath)
         {
